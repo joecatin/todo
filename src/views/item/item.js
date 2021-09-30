36d8723e9821cf73c5d45f8479d067a1c5700564
bautheac <o.bautheac@gmail.com>
2021-09-30 10:56:39 +0100

    Make item collapsible

--- src/views/item/item.js
+++ src/views/item/item.js
@@ -2,31 +2,91 @@
 
 import './item.css';
 
+const makeElement = (type, id, content) => {
+  const element = document.createElement(type);
+  element.id = id;
+  element.textContent = content;
+  return element;
+};
+
+const makeElementCollapsible = (element, type) => {
+  element.addEventListener('click', () => {
+    element.classList.toggle(`${type}-active`);
+    const content = element.nextElementSibling;
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+    } else {
+      content.style.display = 'block';
+    }
+  });
+  return element;
+};
+
+const animateCollapsible = (element, type) => {
+  element.addEventListener('click', () => {
+    element.classList.toggle(`${type}-active`);
+    const content = element.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = `${content.scrollHeight}px`;
+    }
+  });
+  return element;
+};
+
+const makeTitle = (text, type) => {
+  let title = makeElement(
+    'button', `${type}-title`, text,
+  );
+  title = makeElementCollapsible(title, 'item');
+  title = animateCollapsible(title, 'item');
+  return title;
+};
+
+const makeDetails = (type, date, level) => {
+  const details = document.createElement('div');
+  details.id = `${type}-details`;
+  const dueDate = makeElement(
+    'div', `${type}-dueDate`, date,
+  );
+  details.appendChild(dueDate);
+  const priority = makeElement(
+    'div', `${type}-priority`, level,
+  );
+  details.appendChild(priority);
+  return details;
+};
+
+const makeContent = (type, text, ...args) => {
+  const content = document.createElement('div');
+  content.id = `${type}-content`;
+  const description = makeElement(
+    'div', `${type}-description`, text,
+  );
+  content.appendChild(description);
+
+  const details = makeDetails(type, ...args);
+  content.appendChild(details);
+
+  return content;
+};
+
 const showItem = (item) => {
   const view = document.createElement('div');
   view.id = 'item';
 
-  const title = document.createElement('div');
-  title.id = 'title';
-  title.textContent = item.get('title');
+  const title = makeTitle(item.get('title'), 'item');
   view.appendChild(title);
 
-  const description = document.createElement('div');
-  description.id = 'description';
-  description.textContent = item.get('description');
-  view.appendChild(description);
+  const content = makeContent(
+    'item',
+    item.get('description'),
+    item.get('dueDate'),
+    item.get('priority'),
+  );
 
-  const details = document.createElement('div');
-  details.id = 'details';
-  const dueDate = document.createElement('div');
-  dueDate.id = 'dueDate';
-  dueDate.textContent = item.get('dueDate');
-  details.appendChild(dueDate);
-  const priority = document.createElement('div');
-  priority.id = 'priority';
-  priority.textContent = item.get('priority');
-  details.appendChild(priority);
-  view.appendChild(details);
+  view.appendChild(content);
 
   return view;
 };
